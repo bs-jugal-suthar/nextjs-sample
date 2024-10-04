f6a3ecb1d5eb91838276c232a5eeab1abc46a0c5
jugalsuthar01 <jugal.suthar@bombaysoftwares.com>
2024-10-04 12:42:38 +0530

    layout

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Layout = ({children}:{children:React.ReactNode}) => {
   return (
-    <aside className='bg-gray-600'> 
-        {children}
-    </aside>
+   <div className='flex flex-row w-screen h-screen'>
+    <div className='w-400 h-screen bg-fuchsia-400'>hello</div>
+    <div>{children}</div>
+   </div>
   )
 }
 
